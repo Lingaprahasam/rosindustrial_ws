af4668c3f977eb3894bd09565e97a0764cc3f2e1
Lingaprahasam <prahas.programming@gmail.com>
2018-02-19 21:31:23 +0100

    renamed my_workcell to myworkcell and added myworkcell_support

--- src/myworkcell/src/myworkcell_node.cpp
+++ src/myworkcell/src/myworkcell_node.cpp
@@ -9,11 +9,16 @@ public:
         vision_client_ = nh.serviceClient<my_workcell::LocalizePart>("localize_part");
     }
 
-    void start()
+    void start(const std::string& base_frame)
     {
-        ROS_INFO("Attempting to localize part");
+        ROS_INFO("Attempting to localize part");        
         // Localize the part
         my_workcell::LocalizePart srv;
+
+        // Using Parameter to request
+        srv.request.base_frame = base_frame;
+        ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
+
         if (!vision_client_.call(srv))
         {
             ROS_ERROR("Could not localize part");
@@ -30,15 +35,23 @@ private:
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "myworkcell_node");
+    std::string base_frame;
+
+    ros::init(argc, argv, "my_workcell_node");
     ros::NodeHandle nh;
+
+    // Private Node Handle
+    ros::NodeHandle private_node_handle ("~");
+
+    // parameter name, string object reference, default value
+    private_node_handle.param<std::string>("base_frame", base_frame, "world"); 
 
     ROS_INFO("ScanNPlan node has been initialized");
 
     ScanNPlan app(nh);
 
     ros::Duration(.5).sleep();  // wait for the class to initialize
-    app.start();
+    app.start(base_frame);
 
     ros::spin();
 }
